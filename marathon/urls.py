3ea78ba0469850b91ab609dfa47bbb273a1bfd59
Romeno <RomenoEx@gmail.com>
2018-07-02 03:20:25 +0300

    static, media

--- marathon/urls.py
+++ marathon/urls.py
@@ -15,6 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, re_path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     re_path(r'admin/', admin.site.urls),
@@ -34,3 +37,6 @@ urlpatterns = [
     re_path(r'^tasks/', include('marathon_task.urls')),
     re_path(r'^signals/', include('marathon_signal.urls')),
 ]
+
+# if settings.DEBUG:
+#     urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
