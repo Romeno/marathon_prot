84ae635c650882c2efd516fc206a5d300d1e132e
Romeno <RomenoEx@gmail.com>
2018-08-11 16:06:22 +0300

    fixtures

--- scripts/dumpdata.bat
+++ scripts/dumpdata.bat
@@ -1,7 +1,11 @@
 @echo off
 pushd I:\Romeno\Projects\PyCharm\marathon
 python ./manage.py dumpdata auth.user --indent 4 --output fixtures\initial_data.json
+
 python ./manage.py dumpdata marathon_marathons --indent 4 --output marathon_marathons\fixtures\initial_data.json
+
+python ./manage.py dumpdata marathon_announcement --indent 4 --output marathon_announcement\fixtures\initial_data.json
+python ./manage.py dumpdata marathon_chronotrack --indent 4 --output marathon_chronotrack\fixtures\initial_data.json
 
 python ./manage.py dumpdata marathon_common --indent 4 --output marathon_common\fixtures\initial_data.json
 python ./manage.py dumpdata marathon_expo --indent 4 --output marathon_expo\fixtures\initial_data.json
